a47a221d41d6babd7a955efff6b44fd0e448d271
DESKTOP-UN6F40J\AOC <jy02537333@gmail.com>
2025-11-30 22:05:31 +0800

    chore(ui): wire shared ui-thumbnail via runtime re-exports to avoid cross-project tsc inclusion

--- admin-fe/src/components/Thumbnail.tsx
+++ admin-fe/src/components/Thumbnail.tsx
@@ -1,1 +1,3 @@
-export { default } from '../../../packages/ui-thumbnail/react';
+// Use runtime require to avoid TypeScript including package source in admin-fe compilation
+const _shared = require('../../../packages/ui-thumbnail/react') as any;
+export default (_shared && _shared.__esModule) ? _shared.default : _shared;
--- wx-fe/src/components/Thumbnail.tsx
+++ wx-fe/src/components/Thumbnail.tsx
@@ -1,1 +1,3 @@
-export { default } from '../../../packages/ui-thumbnail/taro';
+// runtime require to avoid including package TS sources in wx-fe tsc program
+const _shared = require('../../../packages/ui-thumbnail/taro') as any;
+export default (_shared && _shared.__esModule) ? _shared.default : _shared;
